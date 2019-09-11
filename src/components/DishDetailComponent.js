75967ec00e5875d96c472632574be2ca99cb5808
Nisha Joshi <nisha.joshi@sweetli.me>
2019-09-11 10:14:45 +0530

    React

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,8 @@
   import { Link } from 'react-router-dom';
   import {Control, LocalForm, Errors} from 'react-redux-form';
   import { Loading } from './LoadingComponent';
+  import { baseUrl } from '../shared/baseUrl';
+  import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
   const required = (val) => val && val.length;
   const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -29,7 +31,7 @@
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
     }
 
     render() {
@@ -100,35 +102,45 @@
       function RenderDish({dish}) {
              return (
                <div className="col-12 col-md-5 m-1">
+                 <FadeTransform 
+                  in 
+                  transformProps = {{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                 }}>
                  <Card>
-                      <CardImg src={dish.image} alt={dish.name} />
+                      <CardImg src={baseUrl + dish.image} alt={dish.name} />
                       <CardBody>
                        <CardTitle>{dish.name}</CardTitle>
                        <CardText>{dish.description}</CardText>
                      </CardBody>
                  </Card>
+                 </FadeTransform>
                 </div>
+                
              );
       }
 
-      function RenderComments({comments, addComment, dishId}) {
+      function RenderComments({comments, postComment, dishId}) {
           if (comments != null)
             return(
               <div className="col-12 col-md-5 m-1">
               <h4>Comments</h4>
               <ul className="list-unstyled">
-              {comments.map((comments) => {
-                return (
-                  <li key={comments.id}>
-                  <p>{comments.comment}</p>
-                  <p>-- {comments.author}, {new Intl.DateTimeFormat('en-US',
-                  {year:'numeric', month:'short',day:'2-digit'}).format(new Date(Date.parse(comments.date)))}</p>
-                  </li>
-                );
-              })
-            }
+                <Stagger in>
+                  {comments.map((comments) => {
+                    return (
+                      <Fade in>
+                      <li key={comments.id}>
+                      <p>{comments.comment}</p>
+                      <p>-- {comments.author}, {new Intl.DateTimeFormat('en-US',
+                      {year:'numeric', month:'short',day:'2-digit'}).format(new Date(Date.parse(comments.date)))}</p>
+                      </li>
+                      </Fade>
+                    );
+                  })}
+                </Stagger>
             </ul>
-            <CommentForm dishId={dishId} addComment={addComment}/>
+            <CommentForm dishId={dishId} postComment={postComment}/>
           </div>
           );
 
@@ -177,7 +189,7 @@
               <div className="row">
              <RenderDish dish = {props.dish} />
               <RenderComments comments = {props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               dishId={props.dish.id} />
               </div>
               </div>
